e8adeb085254f04b8bb0ba0af223a36725d7d7d0
goore3 <kowalik.twoja1@gmail.com>
2022-05-17 16:24:42 +0200

    Add few changes to calendar

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,6 +1,6 @@
 class Animal < ApplicationRecord
   belongs_to :enclosure
-  has_many :note
+  has_many :note, dependant: :delete_all
   validates :age, numericality: { greater_than_or_equal_to: 0 }, presence: true
   validates :name, presence: true
   validates :species, presence: true
