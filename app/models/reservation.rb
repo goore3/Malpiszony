c62d1b556912fbdc15549752ec3161fb8d785f75
goore3 <kowalik.twoja1@gmail.com>
2022-06-21 18:39:22 +0200

    Add style for new public reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,4 +4,11 @@ class Reservation < ApplicationRecord
   validates :last_name, presence: true, length: {minimum: 2, maximum: 25}
   validates :phone, presence: true, length: {is:9}, format: {with: /\A[+-]?\d+\z/, message: "only allows numbers"} 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validate :oldEvent
+
+  def oldEvent
+    if event.start_time < Date.today
+      errors.add(:event,"się odbywa lub odbyło")
+    end
+  end
 end
