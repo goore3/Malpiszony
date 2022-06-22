dc4bcbb4ef01dd8c071a49d07113c29286e74cc7
goore3 <kowalik.twoja1@gmail.com>
2022-06-22 09:05:57 +0200

    Add admin and remove employee icon

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.action_mailer.delivery_method = :test
+  config.action_mailer.delivery_method = :smtp
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
 
